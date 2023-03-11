47fa46f6967b56c46e20d6c4e64511b9f6d03918
ShubhamKrSingh97 <shubhkrsingh.1997@gmail.com>
2023-03-11 18:33:09 +0530

    delete functionality from UI and LS added

--- lets.js
+++ lets.js
@@ -1,32 +1,52 @@
-var btn=document.querySelector('button');
-var nameInput=document.querySelector('#name');
-var emailInput=document.querySelector('#email');
-var numInput=document.querySelector('#number');
-btn.addEventListener('click',(e)=>{
+var btn = document.querySelector('button');
+var nameInput = document.querySelector('#name');
+var emailInput = document.querySelector('#email');
+var numInput = document.querySelector('#number');
+btn.addEventListener('click', (e) => {
     e.preventDefault();
-    var user=document.querySelector('.user');
-    var n=document.createElement('li');
-    n.textContent=nameInput.value;
+    var user = document.querySelector('.user');
+    var n = document.createElement('li');
+    n.textContent = nameInput.value + "-" + emailInput.value + "-" + numInput.value;
     user.appendChild(n);
-    var e=document.createElement('li');
-    e.textContent=emailInput.value;
-    user.appendChild(e);
-    var nu=document.createElement('li');
-    nu.textContent=numInput.value;
-    user.appendChild(nu);
-    
-    var obj={
-        name:nameInput.value, email: emailInput.value, number: numInput
+    var btn = document.createElement('button'); btn.setAttribute('class', 'del');
+    btn.innerText = 'Delete';
+    n.appendChild(btn);
 
+    var obj = {
+        name: nameInput.value, email: emailInput.value, number: numInput
     }
-    localStorage.setItem(`${numInput.value}`,JSON.stringify(obj));
+    localStorage.setItem(obj.email, JSON.stringify(obj));
+    user.addEventListener('click', (e) => {
+        e.preventDefault;
+        if (e.target.classList.contains('del')) {
+            e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+            var text = e.target.parentNode.textContent;
+            console.log(e.target.parentNode.textContent);
+            var key = '';
+            var count = 0;
+            for (var i = 0; i < text.length; i++) {
+                if (text[i] == '-') {
+                    if (count == 1) {
+                        break;
+                    }
+                    count++;
+                    key = '';
+                }
+                else {
+                    key += text[i];
+                }
 
-    
+            }
+            localStorage.removeItem(key);
+        }
+    })
+
+
 });
 
-const form=document.querySelector('.info');
+const form = document.querySelector('.info');
 
-const empty=document.querySelector('.empty');
+const empty = document.querySelector('.empty');
 
 
 
