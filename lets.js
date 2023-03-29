67e2843b47106213ab8c869c6b7323862ba2de7f
ShubhamKrSingh97 <shubhkrsingh.1997@gmail.com>
2023-03-29 13:04:11 +0530

    Implemented the data loading on refresh from crud

--- lets.js
+++ lets.js
@@ -4,36 +4,45 @@ var emailInput = document.querySelector('#email');
 var numInput = document.querySelector('#number');
 btn.addEventListener('click', (e) => {
     e.preventDefault();
+    var obj = {
+        name: nameInput.value, email: emailInput.value, number: numInput.value
+    }
+    display(obj);
+    axios.post("https://crudcrud.com/api/5cb2b7e480c5413cb54e47d54f44a8ee/appointmentData", obj);
+
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get("https://crudcrud.com/api/5cb2b7e480c5413cb54e47d54f44a8ee/appointmentData").then((response) => {
+        for (var i = 0; i < response.data.length; i++) {
+             display(response.data[i]);
+        }
+    }).catch(err=>{console.log(err)})
+})
+
+function display(res) {
     var user = document.querySelector('.user');
     var n = document.createElement('li');
-    n.textContent = nameInput.value + "-" + emailInput.value + "-" + numInput.value;
+    n.textContent = res.name + "-" + res.email + "-" + res.number;
     user.appendChild(n);
-    var edit=document.createElement('button');
-    edit.textContent='Edit';
+    var edit = document.createElement('button');
+    edit.textContent = 'Edit';
     var delbtn = document.createElement('button');
     delbtn.innerText = 'Delete';
     n.appendChild(delbtn);
     n.appendChild(edit);
-
-    var obj = {
-        name: nameInput.value, email: emailInput.value, number: numInput.value
-    }
-    localStorage.setItem(obj.email, JSON.stringify(obj));
-    delbtn.addEventListener('click', () => {
+    delbtn.addEventListener('click', (e) => {
         e.preventDefault;
-            user.removeChild(n);
-            localStorage.removeItem(obj.email);
-    });
-    edit.addEventListener('click',()=>{
-        e.preventDefault();
-        nameInput.value=obj.name;
-        emailInput.value=obj.email;
-        numInput.value=obj.number;
         user.removeChild(n);
-        localStorage.removeItem(obj.email);
+    });
+    edit.addEventListener('click', (e) => {
+        e.preventDefault();
+        nameInput.value = res.name;
+        emailInput.value = res.email;
+        numInput.value = res.number;
+        user.removeChild(n);
     })
-});
-
+}
 
 
 
